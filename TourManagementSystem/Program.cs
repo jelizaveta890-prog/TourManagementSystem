dc68cb2b694932409c851f2913d27068ad439d74
jelizaveta890-prog <jelizaveta890@gmail.com>
2026-05-15 15:31:57 +0300

    LanguageController_Style

--- TourManagementSystem/Program.cs
+++ TourManagementSystem/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TourManagementSystem;
+using Microsoft.AspNetCore.Localization;
+using System.Globalization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,18 +12,40 @@ var connectionString = builder.Configuration.GetConnectionString("TourManagement
 builder.Services.AddDbContext<TourManagementSystemContext>(options =>
     options.UseSqlServer(connectionString));
 
-// 2. Teenuste lisamine
-builder.Services.AddControllersWithViews();
+// 2. KEELTE SEADISTAMINE (Lokaliseerimine)
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+
+builder.Services.AddControllersWithViews()
+    .AddViewLocalization()
+    .AddDataAnnotationsLocalization();
+
+builder.Services.Configure<RequestLocalizationOptions>(options =>
+{
+    // Määrame toetatud keeled, 'en' on esimene (vaikevalik)
+    var supportedCultures = new[] {
+        new CultureInfo("en"),
+        new CultureInfo("et"),
+        new CultureInfo("ru")
+    };
+
+    options.DefaultRequestCulture = new RequestCulture("en"); // Alustab alati inglise keeles
+    options.SupportedCultures = supportedCultures;
+    options.SupportedUICultures = supportedCultures;
+
+    // EEMALDAME kõik muud pakkujad (nagu brauseri seaded) 
+    // ja jätame AINULT küpsise (Cookie), et sinu valik jääks püsima.
+    options.RequestCultureProviders.Clear();
+    options.RequestCultureProviders.Add(new CookieRequestCultureProvider());
+});
 
 var app = builder.Build();
 
-// 3. AUTOMAATNE ANDMEBAASI LOOMINE (Sinu lisatud osa, nüüd parandatud)
+// 3. AUTOMAATNE ANDMEBAASI LOOMINE
 using (var scope = app.Services.CreateScope())
 {
     var services = scope.ServiceProvider;
     try
     {
-        // Eemaldasime siit vahelt ".Data"
         var context = services.GetRequiredService<TourManagementSystemContext>();
         context.Database.EnsureCreated();
     }
@@ -39,13 +63,19 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // Lisame kindluse mõttes ka selle
 app.UseRouting();
+
+// AKTIVEERI KEELTE KASUTAMINE (peab olema enne Authorizationit)
+app.UseRequestLocalization();
+
 app.UseAuthorization();
 app.MapStaticAssets();
 
+// 5. MARSRUUTIMINE (Suunab kohe Tours lehele)
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Tours}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 app.Run();
