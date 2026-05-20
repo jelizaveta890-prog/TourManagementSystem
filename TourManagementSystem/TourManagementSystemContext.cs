5b92c515084b5fba5a2500d446b33b9dc52744a8
lilchikkk <lil.chiiiik@gmail.com>
2026-05-20 20:34:03 +0300

    Db set add
    chande db

--- TourManagementSystem/TourManagementSystemContext.cs
+++ TourManagementSystem/TourManagementSystemContext.cs
@@ -3,12 +3,5 @@ using TourManagementSystem.Models;
 
 public class TourManagementSystemContext(DbContextOptions<TourManagementSystemContext> options) : DbContext(options)
 {
-    public TourManagementSystemContext(DbContextOptions<TourManagementSystemContext> options, bool AutoCreate = true) : this(options)
-    {
-        // delete and recreate the database on startup for testing purposes
-        Database.EnsureDeleted();
-        Database.EnsureCreated();
-    }
-
     public DbSet<TourManagementSystem.Models.Tour> Tour { get; set; } = default!;
 }
