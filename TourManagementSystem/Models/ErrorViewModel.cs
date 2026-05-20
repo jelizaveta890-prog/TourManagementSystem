d1994f35274ccd3078680317d275a719cf7d55fb
lilchikkk <lil.chiiiik@gmail.com>
2026-05-20 19:43:56 +0300

    comit for fix

--- TourManagementSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace TourManagementSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
